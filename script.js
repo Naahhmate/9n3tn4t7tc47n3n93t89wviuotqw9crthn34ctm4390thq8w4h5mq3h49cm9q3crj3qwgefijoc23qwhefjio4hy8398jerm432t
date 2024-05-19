4b799fc03ac74bbf6f847c5abb2d130ccc1ec3e9
Naahhmate <116361088+Naahhmate@users.noreply.github.com>
2024-05-19 13:38:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -205,9 +205,7 @@ function throttle(fn, delay) {
     title.innerText = message
     content.innerText = cont
     overlay.style.display='flex';
-    if (isMobile) {
-        mainContainer.style.opacity='1';
-    }
+
     if (type == "Normal") {
         icon.className = "fa-solid fa-circle-info"
         const icons = document.querySelectorAll('.modal-box i');
